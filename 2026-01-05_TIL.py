106d43dbb3622d271ec1bf8d621db7487a8e84c2
t.setoguchi <t.setoguchi2611@gmail.com>
2026-01-05 20:49:06 +0900

    学習ログ:pandasを使用し簡潔、高速化

--- 2026-01-05_TIL.py
+++ 2026-01-05_TIL.py
@@ -5,13 +5,22 @@ broken_sales_data = [
     {"item": "バナナ", "price": 200, "category": "fruit"},
 ]
 
-def get_category_total_safe(data, category_name):
-    total = 0
-    for row in data:
-        if row.get("category") == category_name:
-            # ここで price があるかチェックしてから足したい
-            # ヒント：row.get("price", 0) を使うと、なければ 0 を返してくれます
-            total += row.get("price", 0)
-    return total
+# def get_category_total_safe(data, category_name):
+#     total = 0
+#     for row in data:
+#         if row.get("category") == category_name:
+#             # ここで price があるかチェックしてから足したい
+#             # ヒント：row.get("price", 0) を使うと、なければ 0 を返してくれます
+#             total += row.get("price", 0)
+#     return total
 
-print(f"安全な集計結果: {get_category_total_safe(broken_sales_data, 'fruit')}")
+# print(f"安全な集計結果: {get_category_total_safe(broken_sales_data, 'fruit')}")
+
+import pandas as pd
+
+df = pd.DataFrame(broken_sales_data)
+print(df)
+
+
+result = df[df["category"] == "fruit"]["price"].sum()
+print(result)
